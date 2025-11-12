4d30207d36773e8c3f84b16034ef46bc83a7227f
IamAnkit19 <ankit003322@gmail.com>
2025-11-12 17:03:01 +0530

    Day 46 Otp Generation

---
+++ Day46/Server/twilioServices.js
@@ -0,0 +1,15 @@
+// const twilio = require('twilio');
+// const accountSID = 'Your SID...';
+// const authToken = 'Tour Auth Token...';
+
+// const client = new twilio(accountSID, authToken);
+
+// const sendOtp = (phoneNumber, otp) => {
+//     return client.messages.create({
+//         body: `Your OTP is ${otp}`,
+//         from: 'Your purchased number...',
+//         to: phoneNumber
+//     });
+// };
+
+// module.exports = sendOtp;
