b46d8d04723cbd938e1594785b120c6d1c0188f3
IamAnkit19 <ankit003322@gmail.com>
2025-11-17 17:35:33 +0530

    Day 49 Password Reset Using Email

--- Day49/Client/LoginSignUp/src/ForgetPassword.jsx
+++ Day49/Client/LoginSignUp/src/ForgetPassword.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react'
 import { useState } from 'react';
 
@@ -8,13 +9,24 @@ const ForgetPassword = () => {
     function fun1(e){
         let {name, value} = e.target;
         setInput({...input,[name]:value})
-        // console.log(input);
+        console.log(input);
+    }
+    async function fun2(){
+        try{
+            let res = await axios.post('http://localhost:4000/forget-password',input);
+            alert(res.data)
+        }
+        catch(error){
+            alert(error)
+            console.log(error);
+        }
     }
   return (
     <div className='mainDiv'>
         <div className='fields'>
             <h1>Forget Password</h1>
-            <input style={{width:"50vw"}} type="email" id='Femail' name='email' value={input.email} onChange={fun1} placeholder='Enter your Email'/>
+            <input style={{width:"380px"}} type="email" id='Femail' name='email' value={input.email} onChange={fun1} placeholder='Enter your Email'/>
+            <button onClick={fun2} id='btn'>Send OTP</button>
         </div>
     </div>
   )
