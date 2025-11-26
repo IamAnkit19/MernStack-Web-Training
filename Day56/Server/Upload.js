a6220ae8e6c8834ad09e742d77420ae70c77bc9d
IamAnkit19 <ankit003322@gmail.com>
2025-11-26 12:01:24 +0530

    Day 56 Like Feature

---
+++ Day56/Server/Upload.js
@@ -0,0 +1,27 @@
+let mongoose = require('mongoose');
+let imageSchema = mongoose.Schema({
+    // name:{
+    //     type:String
+    // },
+    imgUrl:{
+        type:String,
+        required:true
+    },
+    likeCount:{
+        type:Number,
+        default:0
+    },
+    user: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",   // reference to user model
+        required: true
+    }],
+    likedBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    }]
+
+})
+
+let Upload = mongoose.model('UploadImage',imageSchema);
+module.exports = Upload;
